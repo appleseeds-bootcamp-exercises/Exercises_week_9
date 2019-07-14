65b7b8020400ec72b50537ad7e294d57b6b09706
Sapir Malka <sapirm290@gmail.com>
2019-07-14 11:44:18 +0300

    Create star1..py

---
+++ python-environment/star1..py
@@ -0,0 +1,12 @@
+# help('modules')
+import math, email, os
+from enum import Flag
+from random import randint, choice, shuffle
+from queue import *
+from math import ceil as ceiling
+from math import exp as exponent
+from math import factorial as calcFactorial
+
+assert(calcFactorial(5) == 120)
+print(exponent(3))
+print(ceiling(3.2))
